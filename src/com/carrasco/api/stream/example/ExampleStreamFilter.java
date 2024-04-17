acfb88e52ebc36ea5814fd986c1eebb782cdb36e
pedro <pedro.cm.1989@gmail.com>
2024-04-17 16:30:36 +0200

    add filter

--- src/com/carrasco/api/stream/example/ExampleStreamFilter.java
+++ src/com/carrasco/api/stream/example/ExampleStreamFilter.java
@@ -6,18 +6,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ExampleStreamMap {
+public class ExampleStreamFilter {
     public static void main(String[] args) {
 
         Stream<User> name = Stream
-                .of("Luis Cobos", "Peter Pan","Fedor Emilianenco","Mircro Crocop")
+                .of("Luis Cobos", "Peter Pan","Fedor Emilianenco","Mircro Crocop",
+                        "Luis Melgar", "Luis Carrasco")
                 .map(nombre->new User(nombre.split(" ")[0],nombre.split(" ")[1]))
-                .peek(u -> System.out.println(u.getName()))
-                .map((user -> {
-                    String n = user.getName();
-                    user.setName(n);
-                    return user;
-                }));
+                .filter(u-> u.getName().equalsIgnoreCase("luis"))
+                .peek(u -> System.out.println(u.getName()));
 
         List<User> list = name.collect(Collectors.toList());
         list.forEach(u -> System.out.println(u.getSecondname()));
--- src/com/carrasco/api/stream/example/models/User.java
+++ src/com/carrasco/api/stream/example/models/User.java
@@ -1,2 +1,41 @@
-package com.carrasco.api.stream.example.models;public class User {
+package com.carrasco.api.stream.example.models;
+
+public class User {
+
+    private String name;
+    private String secondname;
+    private Integer id;
+     private static int ultimoId;
+
+    public User(String name, String secondname) {
+        this.name = name;
+        this.secondname = secondname;
+        this.id= ultimoId++;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondname() {
+        return secondname;
+    }
+
+    public void setSecondname(String secondname) {
+        this.secondname = secondname;
+    }
+
+    @Override
+    public String toString() {
+        return  "name='" + name + '\'' +
+                ", secondname='" + secondname ;
+    }
 }
